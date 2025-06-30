b865b7bf3cac59b1842f0ece3bf9ae5f099324bd
ryandd <ryandwikidermawan06@gmail.com>
2025-06-30 23:36:29 +0700

    course 2:16

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -9,10 +9,16 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe public fundMe;
 
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether; // 0.1 ETH
+    uint256 constant STARTING_BALANCE = 10 ether; // 10 ETH
+
+
     function setUp() external {
         // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE); // give USER 10 ETH
     }
 
     function testMinimumDollarIsFive() public view {
@@ -34,5 +40,24 @@ contract FundMeTest is Test {
             uint256 version = fundMe.getVersion();
             assertEq(version, 4);
         }
-    } 
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert(); // the next line, should revert
+        fundMe.fund(); // this tx fails/reverts
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER); // make the next tx come from USER
+        fundMe.fund{value: SEND_VALUE}();
+        // Check that the amount funded by USER is correct
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testWithdrawFailsIfNotOwner() public {
+        vm.expectRevert(); // the next line, should revert
+        vm.prank(USER); // make the next tx come from USER
+        fundMe.withdraw(); // this tx fails/reverts
+    }
 }
